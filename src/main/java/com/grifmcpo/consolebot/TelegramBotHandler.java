2192124a88499ae93d6c2e61044fa96ca9e5a85e
GrifMcPo <brawltvink6@bk.ru>
2026-08-02 20:41:28 +0300

    Update TelegramBotHandler.java

--- src/main/java/com/grifmcpo/consolebot/TelegramBotHandler.java
+++ src/main/java/com/grifmcpo/consolebot/TelegramBotHandler.java
@@ -6,7 +6,7 @@ import org.telegram.telegrambots.bots.TelegramLongPollingBot;
 import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.methods.updatingmessages.DeleteMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
-import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException; 
 
 import java.io.File;
 import java.text.SimpleDateFormat;
